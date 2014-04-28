4ce74bff401e397097178f0722fc9453e4034d03
Ahmed Nadar <ahmedfay@hotmail.com>
2014-04-28 19:15:26 -0400

    Pass 02

---
+++ 02_baby_steps/02_baby_steps.js
@@ -0,0 +1,10 @@
+// 	REQUIRNMENT
+// rite a program that accepts one or more numbers as command-line arguments and prints the sum of those numbers to the console (stdout).
+
+// 	SOLUTION
+
+var sum = 0
+for (var i = 2; i < process.argv.length ; i++) {
+	sum += Number(process.argv[i])
+};
+console.log(sum)
